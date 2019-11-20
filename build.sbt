517b805a34203d34f375b81143e7452c42910c65
lujeremy <jeremy.lu@stern.nyu.edu>
2019-11-19 23:05:37 -0500

    Initial rest call

--- build.sbt
+++ build.sbt
@@ -2,4 +2,4 @@ name := "CoinFriend"
 version := "0.1"
 scalaVersion := "2.13.1"
 
-libraryDependencies += "com.konghq" % "unirest-java" % "3.1.04" % "provided"
+libraryDependencies += "com.konghq" % "unirest-java" % "3.1.02"
--- src/main/scala/CoinFriend.scala
+++ src/main/scala/CoinFriend.scala
@@ -1,9 +1,32 @@
+import kong.unirest.Unirest
+
+import scala.io.Source
+import scala.util.Try
+
 object CoinFriend {
 
   def main(args: Array[String]): Unit = {
     println("foo")
-    println("foo")
-    println("foo")
-    println("foo")
+    val token = getToken("token.txt")
+    token.foreach(t => fetch(t))
   }
+
+  def getToken(filename: String): Option[String] = {
+    Try {
+      val bufferedSource = Source.fromFile(filename)
+      val token = bufferedSource.getLines().mkString
+      bufferedSource.close()
+      token
+    }.toOption
+  }
+
+  def fetch(token: String): Unit = {
+    val response = Unirest
+      .get("https://rest.coinapi.io/v1/quotes/CHAOEX_SPOT_BTC_USDT/current")
+      .header("accept", "application/json")
+      .header("X-CoinAPI-Key", token)
+      .asJson()
+    print(response.getBody.toString)
+  }
+
 }
